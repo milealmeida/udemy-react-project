1e9d7d8a669751f2d003e2fd6de6418f8f06aba3
Milena <milenaalmeidadev@hotmail.com>
2021-09-22 09:34:13 -0400

    refactor: create page About and use destructuring props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
 import Home from './Home';
+import About from './About';
 
 function App() {
   const home = 'House';
   const pi = 3.14;
 
+  const message = 'Welcome, this is About page.'
+
   return (
     <div className="App">
       Milena
       <Home home={home} pi={pi}/>
+      <About message={message}/>
     </div>
   );
 }
