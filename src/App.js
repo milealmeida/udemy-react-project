22523b3aa9a06cb2224ff91de557a1f0efe6700a
Milena <milenaalmeidadev@hotmail.com>
2021-09-24 08:58:21 -0400

    feat: added useEffect to learningo more about how to use and the cases

--- src/App.js
+++ src/App.js
@@ -1,59 +1,26 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
 
-  const [name, setName] = useState('Javascript Developer');
-  const [age, setAge] = useState(45);
+  const [message, setMessage] = useState('Hello');
 
-  const [isDifficult, setIsDifficult] = useState(false);
-  const [onlyTrue, setOnlyTrue] = useState(true);
+  useEffect(() => {
+    alert(message);
+  }, []);
 
-  const [arrary, setArray] = useState(['1', '2', '3']);
-
-  const [objects, setObjects] = useState([
-    { id: 1,name: 'lion' },
-    { id: 2,name: 'tiger' },
-    { id: 3,name: 'fish' },
-    { id: 4,name: 'bird' },
-    { id: 5,name: 'spaceship' },
-  ]); 
-
-  const filteredArray = () => {
-    console.log(objects.filter( ({name}) => name !== 'lion' ));
-  }
+  useEffect(() => {
+    alert('Changed');
+  }, [message]);
 
   return (
     <div className="App">
-      Name: {name}
-      <div>{age}</div>
 
-      <button onClick={() => setName('React JS Developer')}>Change name</button>
       <input 
         type="text" 
-        placeholder="Name"
-        value={name}
-        onChange={ e => setName(e.target.value) } 
+        value={message} 
+        onChange={ e => setMessage(e.target.value) } 
       />
-
-      <button onClick={ () =>  setIsDifficult(!isDifficult) }>Change Difficult</button>
-
-      {isDifficult ? (
-        <div> It's really Difficult </div>
-      ) : (
-        <div> it's not really that difficult </div>
-      )}
-
-      { onlyTrue && (<div> It is really true </div> ) }
-
-      <div>
-        {objects.map(({id, name}) => (
-            <div key={id}>{name}</div>
-          )
-        )}
-      </div>
-
-      <button onClick={filteredArray}>Filter Array</button>
 
     </div>
   );
