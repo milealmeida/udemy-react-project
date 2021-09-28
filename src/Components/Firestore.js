a164808f6101dd55ac6ed98239d0381eb1ad77e9
Milena <milenaalmeidadev@hotmail.com>
2021-09-28 13:19:24 -0400

    feat: printed names at my home with firestore

--- src/Components/Firestore.js
+++ src/Components/Firestore.js
@@ -1,17 +1,26 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import { collection, getDocs } from  'firebase/firestore';
 import { db } from '../Firebase';
 
 const Firestore = () => {
 
+    const [nameData, setNameData] = useState([]);
+
     const getData = async () => {
+        let nameArray = [];
+
         const names = collection(db, 'names');
         const listNames = await getDocs(names);
 
         listNames.forEach((doc) => {
-            console.log(doc.id, '=>', doc.data());
-          });
+            // console.log(doc.id, '=>', doc.data());
+            nameArray.push(doc.data());
+        });
+
+        setNameData(nameArray);
+
+        console.log(nameData);
     };
 
     useEffect(() => {
@@ -21,6 +30,11 @@ const Firestore = () => {
     return (
         <div>
             Firebase Firestore Example.
+            <div>
+                {nameData.map( doc => (
+                    <div>{doc.name}</div>
+                ))}
+            </div>
         </div>
     );
 }
